4501157d77eae8b67a51dcb47bb2e855a347e924
magiclud <you@example.com>
2014-03-22 15:54:12 +0100

    szyfrowanie i deszyfrowanie pliku

--- src/MusicPlayer.java
+++ src/MusicPlayer.java
@@ -1,6 +1,5 @@
 import java.io.File;
 import java.io.IOException;
-
 import javax.sound.sampled.*;
 
 public class MusicPlayer {
@@ -11,37 +10,46 @@ public class MusicPlayer {
 	static String hasloDoKeystora = "ala ma kota";
 	static String aliasHasla = "mojAlias";
 	static String sciezkaDoKeyStore = "D:\\eclipse\\Semestr4\\AES\\keyStore.ks";
-	
+
 	int cipherBytes;
+
+	static String zaszyfrowanyPlik = "D:\\eclipse\\Semestr4\\AES\\plikMuzycznyZaszyfrowany.wav";
+
 	public static void main(String[] args) {
 
-		byte[] cipherBlok = MusicSzyfrowanie.przygotujDoZakodowania(MusicSzyfrowanie.pobierzKlucz(
-				sciezkaDoKeyStore, new String(aliasHasla), new String(
-						hasloDoKeystora)), trybSzyfrowania);
-		
-		
-		
 		try {
-			
-			File file = new File(
-				"D:\\Pobieranie\\14.wav");
-			AudioInputStream outSteream = AudioSystem.getAudioInputStream(file);
+
+			String sciezkaDoPlikuMuzycznego = "D:\\Pobieranie\\14.wav";
+			File plikMuzycznyWav = new File(sciezkaDoPlikuMuzycznego);
+			File plikZaszyfrowany = new File(zaszyfrowanyPlik);
+
+			MusicSzyfrowanie.zaszyfrujPlik(MusicSzyfrowanie.pobierzKlucz(
+					sciezkaDoKeyStore, new String(aliasHasla), new String(
+							hasloDoKeystora)), plikZaszyfrowany,
+					plikMuzycznyWav);
+			MusicSzyfrowanie.dekodujPlik(MusicSzyfrowanie.pobierzKlucz(
+					sciezkaDoKeyStore, new String(aliasHasla), new String(
+							hasloDoKeystora)), plikZaszyfrowany,
+					plikMuzycznyWav);
+
+			AudioInputStream outSteream = AudioSystem
+					.getAudioInputStream(plikMuzycznyWav);
 			clip = AudioSystem.getClip();
 			clip.open(outSteream);
-			
+
 			clip.start();
-	//clip.stop()
+			// clip.stop()
 
-			//do {
-				try {
-					Thread.sleep(5000);
-			
-				} catch (InterruptedException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
+			 do {
+			try {
+				Thread.sleep(5000);
 
-			//} while (clip.isActive());
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+
+			 } while (clip.isActive());
 
 		} catch (LineUnavailableException e) {
 			// TODO Auto-generated catch block
